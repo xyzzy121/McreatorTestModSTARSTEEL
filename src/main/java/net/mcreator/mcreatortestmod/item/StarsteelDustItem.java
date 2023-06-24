7cce836ec785587c1ad55d3a3ba4a38f0fc54ebd
PARADOX <97844313+xyzzy121@users.noreply.github.com>
2023-06-24 19:43:57 -0400

    update

--- src/main/java/net/mcreator/mcreatortestmod/item/StarsteelDustItem.java
+++ src/main/java/net/mcreator/mcreatortestmod/item/StarsteelDustItem.java
@@ -2,10 +2,16 @@
 package net.mcreator.mcreatortestmod.item;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.World;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.block.BlockState;
 
 import net.mcreator.mcreatortestmod.itemgroup.TestItemGroup;
@@ -27,7 +33,8 @@ public class StarsteelDustItem extends McreatorTestModModElements.ModElement {
 
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
-			super(new Item.Properties().group(TestItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
+			super(new Item.Properties().group(TestItemGroup.tab).maxStackSize(64).isImmuneToFire().rarity(Rarity.RARE)
+					.food((new Food.Builder()).hunger(999).saturation(999f).setAlwaysEdible().meat().build()));
 			setRegistryName("starsteel_dust");
 		}
 
@@ -37,8 +44,35 @@ public class StarsteelDustItem extends McreatorTestModModElements.ModElement {
 		}
 
 		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(StarsteelDustItem.block);
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
 		}
 	}
 }
