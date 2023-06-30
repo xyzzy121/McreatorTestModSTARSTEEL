4ccc02f861acc8cb4f62b250f2870a58b3e694c8
PARADOX <97844313+xyzzy121@users.noreply.github.com>
2023-06-30 19:40:06 -0400

    Added Dark Ambrosia.

--- src/main/java/net/mcreator/mcreatortestmod/init/McreatorTestModModPotions.java
+++ src/main/java/net/mcreator/mcreatortestmod/init/McreatorTestModModPotions.java
@@ -17,4 +17,9 @@ import net.mcreator.mcreatortestmod.McreatorTestModMod;
 public class McreatorTestModModPotions {
 	public static final DeferredRegister<Potion> REGISTRY = DeferredRegister.create(ForgeRegistries.POTIONS, McreatorTestModMod.MODID);
 	public static final RegistryObject<Potion> LEVITATION = REGISTRY.register("levitation", () -> new Potion(new MobEffectInstance(MobEffects.LEVITATION, 3600, 0, false, true)));
+	public static final RegistryObject<Potion> AMBROSIA = REGISTRY.register("ambrosia",
+			() -> new Potion(new MobEffectInstance(MobEffects.HEAL, 9999, 255, false, false), new MobEffectInstance(MobEffects.REGENERATION, 9999, 255, false, false), new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 9999, 255, false, false),
+					new MobEffectInstance(MobEffects.WATER_BREATHING, 9999, 255, false, false), new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 9999, 255, false, false), new MobEffectInstance(MobEffects.HEALTH_BOOST, 9999, 255, false, false),
+					new MobEffectInstance(MobEffects.SATURATION, 3600, 255, false, false), new MobEffectInstance(MobEffects.LUCK, 3600, 5, false, false), new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 3600, 255, false, false),
+					new MobEffectInstance(MobEffects.DOLPHINS_GRACE, 3600, 3, false, false), new MobEffectInstance(MobEffects.NIGHT_VISION, 3600, 0, false, false)));
 }
