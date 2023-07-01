7084a9250110bfdffc5b44acc05821020b690651
PARADOX <97844313+xyzzy121@users.noreply.github.com>
2023-06-30 20:11:10 -0400

    remove null paintinga

--- src/main/java/net/mcreator/mcreatortestmod/init/McreatorTestModModPaintings.java
+++ src/main/java/net/mcreator/mcreatortestmod/init/McreatorTestModModPaintings.java
@@ -15,5 +15,4 @@ import net.mcreator.mcreatortestmod.McreatorTestModMod;
 public class McreatorTestModModPaintings {
 	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, McreatorTestModMod.MODID);
 	public static final RegistryObject<PaintingVariant> I_ASSURE_YOU = REGISTRY.register("i_assure_you", () -> new PaintingVariant(53, 37));
-	public static final RegistryObject<PaintingVariant> PAINTINGA = REGISTRY.register("paintinga", () -> new PaintingVariant(16, 16));
 }
